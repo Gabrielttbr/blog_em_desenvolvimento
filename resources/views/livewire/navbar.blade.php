6285cbfbfcb8b77db3a136a5a9517613cef7118a
Gabriel Carlos <ttbr2888@gmail.com>
2024-06-21 14:15:11 -0300

    feat: implementa front do forumlário de busca na página home

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -22,7 +22,7 @@
                 <div class="flex justify-center items-center sm:p-0 sm:m-0 mx-2.5">
                     <h1 class="text-lg text-white font-bebasNeue">EM DESENVOLVIMENTO</h1>
                 </div>
-                <div class="lg:max-w-xl sm:max-w-sm w-full hidden justify-center justify-end items-center sm:flex ">
+                <!--div class="lg:max-w-xl sm:max-w-sm w-full hidden justify-center justify-end items-center sm:flex ">
                     <div class="bg-custom-gray h-full  w-full justify-center items-center flex ">
                         <p class="text-custom-dark-gray">O que você esta procurando?</p>
                     </div>
@@ -32,7 +32,20 @@
                               d="M15.7824 13.833L12.6666 10.7177C12.5259 10.5771 12.3353 10.499 12.1353 10.499H11.6259C12.4884 9.39596 13.001 8.00859 13.001 6.49937C13.001 2.90909 10.0914 0 6.50048 0C2.90959 0 0 2.90909 0 6.49937C0 10.0896 2.90959 12.9987 6.50048 12.9987C8.00996 12.9987 9.39756 12.4863 10.5008 11.6239V12.1332C10.5008 12.3332 10.5789 12.5238 10.7195 12.6644L13.8354 15.7797C14.1292 16.0734 14.6042 16.0734 14.8948 15.7797L15.7793 14.8954C16.0731 14.6017 16.0731 14.1267 15.7824 13.833ZM6.50048 10.499C4.29094 10.499 2.50018 8.71165 2.50018 6.49937C2.50018 4.29021 4.28781 2.49976 6.50048 2.49976C8.71001 2.49976 10.5008 4.28708 10.5008 6.49937C10.5008 8.70852 8.71314 10.499 6.50048 10.499Z"
                         />
                     </svg>
-                </div>
+                </div>-->
+                <form method="POST" action={} class="lg:max-w-xl sm:max-w-sm w-full hidden justify-center justify-end items-center sm:flex"> 
+                    <input type="text" placeholder="O que você esta procurando?"
+                    class="w-full text-center"
+                    />
+                    <button type="submit">     
+                        <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"
+                            class="p-0 m-0 relative right-5 fill-black" fill="black">
+                            <path class="p-0 m-0 fill-black "
+                                d="M15.7824 13.833L12.6666 10.7177C12.5259 10.5771 12.3353 10.499 12.1353 10.499H11.6259C12.4884 9.39596 13.001 8.00859 13.001 6.49937C13.001 2.90909 10.0914 0 6.50048 0C2.90959 0 0 2.90909 0 6.49937C0 10.0896 2.90959 12.9987 6.50048 12.9987C8.00996 12.9987 9.39756 12.4863 10.5008 11.6239V12.1332C10.5008 12.3332 10.5789 12.5238 10.7195 12.6644L13.8354 15.7797C14.1292 16.0734 14.6042 16.0734 14.8948 15.7797L15.7793 14.8954C16.0731 14.6017 16.0731 14.1267 15.7824 13.833ZM6.50048 10.499C4.29094 10.499 2.50018 8.71165 2.50018 6.49937C2.50018 4.29021 4.28781 2.49976 6.50048 2.49976C8.71001 2.49976 10.5008 4.28708 10.5008 6.49937C10.5008 8.70852 8.71314 10.499 6.50048 10.499Z"
+                            />
+                        </svg>
+                    </button>
+                </form>
                 {{--LINKS MENU --}}
                 <ul class="flex justify-center items-center sm:p-0 sm:m-0 mx-2.5">
                     <li class="px-2">
